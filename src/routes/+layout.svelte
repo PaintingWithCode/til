8275441816b126f781c19c0706176688fc00966b
Aravind Murali <11871996+PaintingWithCode@users.noreply.github.com>
2023-05-18 02:02:36 -0400

    feat: Init project

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+	import '../app.css';
+</script>
+
+<div class="min-w-screen h-full min-h-screen">
+	<slot />
+</div>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,1 @@
+<h1 class="pt-12 text-center text-3xl font-bold">Hello world!</h1>
