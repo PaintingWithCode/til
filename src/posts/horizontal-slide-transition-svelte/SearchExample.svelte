f2223a0a625bbb67d8e590135d820451090d10b0
Aravind Murali <11871996+PaintingWithCode@users.noreply.github.com>
2023-06-21 15:43:52 -0400

    post: Horizontal slide transitions in Svelte

---
+++ src/posts/horizontal-slide-transition-svelte/SearchExample.svelte
@@ -0,0 +1,52 @@
+<script lang="ts">
+	import { fade, slide } from 'svelte/transition';
+
+	let isOpen = false;
+	let value = '';
+	$: if (!isOpen) {
+		value = '';
+	}
+</script>
+
+<div
+	class="relative mx-auto mt-2.5 flex h-12 w-fit items-center justify-center border-x border-orange-600 bg-orange-100/40 text-2xl transition-colors hover:bg-orange-100/60"
+	class:open={isOpen}
+>
+	<button
+		class="mu mu-search w-12 leading-none text-orange-600"
+		on:click={() => (isOpen = !isOpen)}
+	/>
+	{#if isOpen}
+		<div in:slide={{ axis: 'x' }} out:slide={{ axis: 'x' }} class="flex w-36 items-center md:w-48">
+			<!-- svelte-ignore a11y-autofocus -->
+			<input
+				autofocus
+				out:fade={{ duration: 100 }}
+				class="max-w-full bg-transparent text-lg font-medium text-orange-600 focus-visible:outline-none"
+				bind:value
+			/>
+		</div>
+		{#if value.length}
+			<div
+				in:slide={{ axis: 'x' }}
+				out:slide={{ axis: 'x' }}
+				class="absolute left-full top-0 flex h-12 w-12 items-center justify-center bg-orange-600 text-orange-100"
+			>
+				<i
+					in:fade={{ duration: 150, delay: 150 }}
+					out:fade={{ duration: 100 }}
+					class="mu mu-arrow-right cursor-not-allowed"
+				/>
+			</div>
+		{/if}
+	{/if}
+</div>
+
+<style>
+	.open {
+		@apply bg-orange-100/60;
+	}
+	.open button:hover::before {
+		content: '\e741';
+	}
+</style>
---
+++ src/posts/horizontal-slide-transition-svelte/horizontal-slide-transition-svelte.md
@@ -0,0 +1,29 @@
+---
+id: 8BvWz9tB
+title: Horizontal slide transitions in Svelte
+date: '2023-06-21'
+topic: svelte
+additionalTags: 
+  - 'javascript'
+isPublished: true
+---
+
+Svelte provides a couple of useful transitions out of the box, but my favourite has always been the slide. In **v3.56**, a horizontal variant was introduced through the `axis` property:
+```svelte
+<script>
+  import slide from "svelte/transition";
+</script>
+
+<div transiton:slide={{axis: "x"}}>
+  I slide in from the right.
+<div>
+```
+
+<script>
+  import SearchExample from './SearchExample.svelte'
+</script>
+
+Here's an example of a searchbar interaction that utilizes it. Click the search icon and start typing:
+<SearchExample/>
+
+ The code for this component can be found [here](https://github.com/PaintingWithCode/til/blob/main/src/posts/horizontal-slide-transition-svelte/SearchExample.svelte).
