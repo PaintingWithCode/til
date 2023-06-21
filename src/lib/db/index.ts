4a483d35617c397e73c500e790e5d6f436f007e2
Aravind Murali <11871996+PaintingWithCode@users.noreply.github.com>
2023-06-21 01:48:21 -0400

    fix: Switch to edge libSQL client for Cloudflare Workers

--- src/lib/db/index.ts
+++ src/lib/db/index.ts
@@ -1,9 +1,13 @@
-import { createClient } from '@libsql/client';
+import { createClient as createEdgeClient } from '@libsql/client/web';
 import { drizzle } from 'drizzle-orm/libsql';
 
 import { DATABASE_AUTH_TOKEN, DATABASE_URL } from '$env/static/private';
 
-const client = createClient({ url: DATABASE_URL, authToken: DATABASE_AUTH_TOKEN });
+// ! To use a local SQLite db in development, uncomment the code below and comment out the edgeClient.
+// import { createClient as createLocalClient } from '@libsql/client';
+// const client = createLocalClient({ url: DATABASE_URL });
+const client = createEdgeClient({ url: DATABASE_URL, authToken: DATABASE_AUTH_TOKEN });
+
 const db = drizzle(client);
 
 export default db;
