89668ab7ddd5734b48ebbb6dbfd5772bdc46d948
Aravind Murali <11871996+PaintingWithCode@users.noreply.github.com>
2023-06-21 09:06:42 -0400

    fix: Fix responsive bugs on small screens

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -3,7 +3,7 @@
 </script>
 
 <header
-	class="sticky top-0 z-20 flex h-16 w-full flex-row items-center justify-between border-b border-dune-800/80 bg-gradient-to-b from-raffia-100/80 via-raffia-100/40 to-raffia-100/20 px-4 transition-colors md:px-8"
+	class="sticky top-0 z-20 flex h-14 w-full flex-row items-center justify-between border-b border-dune-800/80 bg-gradient-to-b from-raffia-100/80 via-raffia-100/40 to-raffia-100/20 px-4 transition-colors md:h-16 md:px-8"
 	class:bg-raffia-100={isGradientDisabled}
 >
 	<a href="/" aria-label="Home" class="flex flex-row items-center space-x-2">
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -11,7 +11,10 @@
 		currentTarget: EventTarget & HTMLDivElement;
 	};
 
+	let innerWidth = 0;
 	let isHeaderGradientDisabled = false;
+
+	$: headerScrollBoundary = innerWidth < 768 ? 12 : 32;
 
 	afterNavigate(() => {
 		document.getElementById('page')?.scrollTo(0, 0);
@@ -20,13 +23,15 @@
 	function onContainerScroll(event: ScrollEvent) {
 		const scrollY = event.currentTarget.scrollTop;
 
-		if (scrollY >= 32) {
+		if (scrollY >= headerScrollBoundary) {
 			isHeaderGradientDisabled = true;
 		} else {
 			isHeaderGradientDisabled = false;
 		}
 	}
 </script>
+
+<svelte:window bind:innerWidth />
 
 {#if !$page.error}
 	<MetaTags
@@ -50,12 +55,12 @@
 
 <div id="page" class="min-w-screen h-screen overflow-y-auto" on:scroll={onContainerScroll}>
 	<Header isGradientDisabled={isHeaderGradientDisabled} />
-	<div class="z-10 min-h-[calc(100vh-6.5rem)]">
+	<div class="z-10 min-h-[calc(100vh-6rem)] md:min-h-[calc(100vh-6.5rem)]">
 		<div class="absolute bottom-0 left-0 right-0 top-0 -z-10 bg-grid-pattern opacity-10" />
 		{#if $page.error}
 			<slot />
 		{:else}
-			<main class="mx-auto flex max-w-2xl flex-col space-y-8 px-2 py-10 md:px-2 lg:px-0">
+			<main class="mx-auto flex max-w-2xl flex-col space-y-8 px-2 py-4 md:px-2 md:py-10 lg:px-0">
 				<slot />
 			</main>
 		{/if}
