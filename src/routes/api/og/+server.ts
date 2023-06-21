ef7f9694655a4a94e3c41dad991ffd2655d8f0f6
Aravind Murali <11871996+PaintingWithCode@users.noreply.github.com>
2023-06-21 03:04:36 -0400

    fix: Try use library default

--- src/routes/api/og/+server.ts
+++ src/routes/api/og/+server.ts
@@ -1,44 +1,37 @@
 import { ImageResponse } from '@ethercorps/sveltekit-og';
-import { error } from '@sveltejs/kit';
+import type { RequestHandler } from '@sveltejs/kit';
 
-function template(title: string) {
-	return `
-	<div tw="flex w-full h-full items-start justify-start">
-		<img
-		tw="absolute top-0 left-0 right-0 bottom-0"
-		height=630
-		width=1200
-		src="https://671c55d4.til-ahu.pages.dev/images/og-template.png"
-
-		/>
-		<h1 tw="text-6xl font-extrabold text-[#1d1d1b] text-left pt-64 px-28 leading-tight">
-			${title}
-		</h1>
-	</div>
+const template = `
+ <div tw="bg-gray-50 flex w-full h-full items-center justify-center">
+    <div tw="flex flex-col md:flex-row w-full py-12 px-4 md:items-center justify-between p-8">
+      <h2 tw="flex flex-col text-3xl sm:text-4xl font-bold tracking-tight text-gray-900 text-left">
+        <span>Ready to dive in?</span>
+        <span tw="text-indigo-600">Start your free trial today.</span>
+      </h2>
+      <div tw="mt-8 flex md:mt-0">
+        <div tw="flex rounded-md shadow">
+          <a href="#" tw="flex items-center justify-center rounded-md border border-transparent bg-indigo-600 px-5 py-3 text-base font-medium text-white">Get started</a>
+        </div>
+        <div tw="ml-3 flex rounded-md shadow">
+          <a href="#" tw="flex items-center justify-center rounded-md border border-transparent bg-white px-5 py-3 text-base font-medium text-indigo-600">Learn more</a>
+        </div>
+      </div>
+    </div>
+  </div>
 `;
-}
-
-const fontFile = await fetch(
-	'https://github.com/rsms/inter/raw/master/docs/font-files/InterDisplay-ExtraBold.woff'
-);
+const fontFile = await fetch('https://og-playground.vercel.app/inter-latin-ext-400-normal.woff');
 const fontData: ArrayBuffer = await fontFile.arrayBuffer();
 
-export function GET({ url }) {
-	const title = url.searchParams.get('title');
-
-	if (title) {
-		const renderedTemplate = template(title);
-
-		return ImageResponse(renderedTemplate, {
-			height: 627,
-			width: 1200,
-			fonts: [
-				{
-					name: 'Inter Latin',
-					data: fontData,
-					weight: 800,
-				},
-			],
-		});
-	} else throw error(400, 'Invalid params');
-}
+export const GET: RequestHandler = async () => {
+	return ImageResponse(template, {
+		height: 400,
+		width: 800,
+		fonts: [
+			{
+				name: 'Inter',
+				data: fontData,
+				weight: 400,
+			},
+		],
+	});
+};
