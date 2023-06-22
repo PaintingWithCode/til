35c65ede96ffe936f2c661664d3612dd47d63389
Aravind Murali <11871996+PaintingWithCode@users.noreply.github.com>
2023-06-22 04:09:22 -0400

    perf: Optimize font loading

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,4 +1,7 @@
 <script lang="ts">
+	import 'microns/fonts/microns.css';
+	import '@fontsource-variable/inter';
+	import '@fontsource/inter-tight/latin-800.css';
 	import '../app.css';
 	import { MetaTags } from 'svelte-meta-tags';
 
